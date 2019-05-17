ba5a24bb7e96cbd05c77c523c9fa2145715488d7
surajitkundu29 <37033977+surajitkundu29@users.noreply.github.com>
2019-05-17 10:00:02 +0200

    Update ssh_controller_main.py

--- ssh_controller_main.py
+++ ssh_controller_main.py
@@ -1,34 +1,38 @@
-import json 
-import boto3 
- 
-def lambda_handler(event, context): 
-    ipranges = [] 
-    with open('whitelist_ips.json') as json_file:   
-        public_ips = json.load(json_file) 
-    for ip in public_ips['SSHPermission']: 
-        iprange = {} 
-        iprange['CidrIp'] = ip['PublicIp'] 
-        iprange['Description'] = ip['Username'] 
-        ipranges.append(iprange) 
-    print(ipranges) 
-    client = boto3.resource('ec2') 
-    security_group = client.SecurityGroup('sg-0632235b8ebd0749e') 
-    security_group.revoke_ingress(IpPermissions=security_group.ip_permissions) 
-    response = security_group.authorize_ingress( 
-        IpPermissions=[ 
-            { 
-                'FromPort': 22, 
-                'IpProtocol': 'TCP', 
-                'IpRanges': ipranges, 
-                'ToPort': 22, 
-                'UserIdGroupPairs': [ 
-                    { 
-                        'GroupId': 'sg-0632235b8ebd0749e' 
-                    } 
-                ] 
-            }, 
-        ] 
-    ) 
-     
-if __name__ == '__main__': 
-    lambda_handler('', '') 
+import json
+import boto3
+import sys
+
+
+def lambda_handler(sg):
+    ipranges = []
+    with open('whitelist_ips.json') as json_file:
+        public_ips = json.load(json_file)
+    for ip in public_ips['SSHPermission']:
+        iprange = {'CidrIp': ip['PublicIp'], 'Description': ip['Username']}
+        ipranges.append(iprange)
+    print(ipranges)
+    client = boto3.resource('ec2')
+    security_group = client.SecurityGroup(sg)
+    security_group.revoke_ingress(IpPermissions=security_group.ip_permissions)
+    response = security_group.authorize_ingress(
+        IpPermissions=[
+            {
+                'FromPort': 22,
+                'IpProtocol': 'TCP',
+                'IpRanges': ipranges,
+                'ToPort': 22,
+                'UserIdGroupPairs': [
+                    {
+                        'GroupId': sg
+                    }
+                ]
+            },
+        ]
+    )
+    return response
+
+
+if __name__ == '__main__':
+    sg = sys.argv[1]
+    response = lambda_handler(sg)
+    print(response)
